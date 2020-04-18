647a960192e29b0d1bc85b5350eb4bc05c2e13ef
marpme <marpme@users.noreply.github.com>
2020-04-18 11:00:54 +0200

    Create hamsi hash functionality

---
+++ hamsi/hamsi.go
@@ -0,0 +1,13 @@
+// Use of this source code is governed by an ISC
+// license that can be found in the LICENSE file.
+
+package hamsi
+
+// #include <sph_hamsi.h>
+import "C"
+
+// Sum creates a hamsi hash of the given bytes and returns exactly 64 bytes.
+func Sum(dst []byte) []byte {
+	// Return something
+	return make([]byte, 0)
+}
