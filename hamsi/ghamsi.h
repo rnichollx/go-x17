0583ed6646f8727c3020b16a3796ed8e4b97bc5a
marpme <marpme@users.noreply.github.com>
2020-04-18 18:42:15 +0200

    Refactor ghamsi c++ wrapper to not globally define the function

--- hamsi/ghamsi.h
+++ hamsi/ghamsi.h
@@ -1,23 +1,14 @@
 #ifndef VERGE_CRYPTO_POW_HAMSI_H
 #define VERGE_CRYPTO_POW_HAMSI_H
 
-#include <stdlib.h>
-#include <stdint.h>
-#include <string.h>
-#include <stdio.h>
-#include "sph_hamsi.h"
+#ifdef __cplusplus
+extern "C" {
+#endif
 
+void HashHamsi(const char *input, char *output);
 
-inline void HashHamsi(const char *input, char *output)
-{
-    sph_hamsi512_context ctx_hamsi;
-    uint32_t hash[16];
-
-    sph_hamsi512_init(&ctx_hamsi);
-    sph_hamsi512 (&ctx_hamsi, input, 64);
-    sph_hamsi512_close(&ctx_hamsi, hash);
-
-    memcpy(output, hash, 64);
+#ifdef __cplusplus
 }
+#endif
 
 #endif // VERGE_CRYPTO_POW_HAMSI_H
